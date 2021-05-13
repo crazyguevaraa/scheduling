31959f844186024ec53a744fa87a38555ee0e420
Aleksey Otrashchenko <80056516+Aleksey2509@users.noreply.github.com>
2021-05-13 19:58:04 +0300

    Add files via upload

--- project/alpha1.1.c
+++ project/alpha1.1.c
@@ -8,6 +8,15 @@
 //int AddressCompareAllocTable (AllocPart* MemorySet1, AllocPart* MemorySet2);
 //void AddressSortAllocTable (AllocPart AllocTable, int Size);
 
+
+
+typedef struct
+{
+	int * point;                                              //указатель на начало области
+	int size;                                                  //размер области
+} AllocPart;
+
+
 //--------------------------------------------------------------
 //структура задачи
 //--------------------------------------------------------------
@@ -17,6 +26,7 @@ typedef struct  task_to_do{
     unsigned int mem;        //количество памяти, нужное для размещения задачи
     unsigned int time_wait;  //время ожидания
     unsigned int status;     //статус
+	int *	 taken_mem;  //указатель на начало памяти, которое заняло данный task 
 }Task;
 
 
@@ -31,11 +41,7 @@ typedef struct Nod
 //--------------------------------------------------------------
 //структура области памяти
 //--------------------------------------------------------------
-typedef struct
-{
-	int * point;                                              //указатель на начало области
-	int size;                                                  //размер области
-} AllocPart;
+
 
 
 
@@ -64,7 +70,16 @@ typedef struct lst
 }List2;
 
 
+void printMemory (int* Memory, int size)
+{
+    int i = 0;
+	printf("[");
+	for (i = 0; i < size - 1; i++)
+	    printf("%d, ", *(Memory + i) );
 
+
+	printf("%d]\n", *(Memory + i + 1) );
+}
 
 List *createList()
 {
@@ -80,7 +95,8 @@ List2 *createList2()
 
 void insertL(List *lst, Task * task)
 {
-	if( !lst -> head){
+	if( !lst -> head)
+	{
 		Node * newN =  (Node *)calloc(1, sizeof(Node));
 		newN -> task = task;
 		newN -> prev = 0;
@@ -148,7 +164,7 @@ void deleteL(List *lst, Node * del)
 		return;
 	if(del == lst -> head && del == lst -> tail)
 	{
-		printf("head-tail\n");
+		//printf("head-tail\n");
 		lst -> head = 0;
 		lst -> tail = 0;
 		free(del);
@@ -158,7 +174,7 @@ void deleteL(List *lst, Node * del)
 	if ( del == lst -> head)
 	{
 		lst -> head = lst -> head -> next;
-		printf("head: %hhu\n", *(lst -> head -> task));
+		//printf("head: %hhu\n", *(lst -> head -> task));
 		lst -> head -> prev = 0;
 		free(del);
 		del = 0;
@@ -234,6 +250,15 @@ void to_add_to_execution (Task* to_execute, List* lst, List* queue)
 };
 
 
+void printAlloctable(AllocPart *Alloctable, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("size of %d part is %d\n",i, (Alloctable + i)->size );
+    }
+}
+
+
 //--------------------------------------------------------------
 //выполнение задачи
 //--------------------------------------------------------------
@@ -242,7 +267,11 @@ void execution (Task* to_do, List* execution, int *time)
 	*time -= to_do -> time_act;
 
     if(time >= 0)
+	{
         to_do -> status = 4;
+		for (int i = 0; i < to_do->mem; i++)
+			(to_do -> taken_mem)[i] = 0;
+	}
     else
     {
         to_do -> status = 3;
@@ -268,7 +297,7 @@ void execution (Task* to_do, List* execution, int *time)
 //--------------------------------------------------------------
 Task * EnterTask (int n)  // Принимает на вход количество задач, возвращает массив структур Task
 {
-	Task * structArray = (Task * )calloc(n, sizeof(Task));
+	Task * structArray = (Task * )calloc(1, n * sizeof(Task));
 
     for(int i = 0; i < n; i++)
     {
@@ -360,18 +389,21 @@ void destroyBothAllocTables (AllocPart* AllocTableFree, AllocPart* AllocTableEmp
 //ПРИНИМАЕТ:  указатель на память, указатель на таблицу аллокации занятых областей
 //ВОЗВРАЩАЕТ: размер занятой области (в ячейках)
 
-int EnterValuesEmployed (int* Memory, AllocPart* AllocTableEmployed)
+int EnterValuesEmployed (int* Memory, int allowedMem ,AllocPart* AllocTableEmployed)
 {
+	int EmployedArea = 0;
 	AllocTableEmployed -> point = Memory;                      //запись в элемент массив структур указателя на начало новой занятой области
-	for (int EmployedArea = 0;;)
+	for (EmployedArea = 0; EmployedArea < allowedMem ;)
 	{
-		if (Memory [EmployedArea + 1] == 1) EmployedArea++; //если область все еще занята, то продолжаем двигаться правее
-		else
+		if (Memory [EmployedArea] == 1)
 		{
-			AllocTableEmployed -> size = EmployedArea; //запись в элемент массива структур размера области
-			return EmployedArea;                       //возврат размера занятой области
+			EmployedArea++; //если область все еще занята, то продолжаем двигаться правее
 		}
+		else
+			break;
 	}
+	AllocTableEmployed -> size = EmployedArea; //запись в элемент массива структур размера области
+			return EmployedArea;                       //возврат размера занятой области
 }
 
 //---------------------------------------------------------------------
@@ -379,18 +411,22 @@ int EnterValuesEmployed (int* Memory, AllocPart* AllocTableEmployed)
 //ПРИНИМАЕТ : указатель на память, указатель на таблицу аллокации пустых областей
 //ВОЗВРАЩАЕТ: размер пустой области (В ячейках)
 
-int EnterValuesFree (int* Memory, AllocPart* AllocTableFree)
+int EnterValuesFree (int* Memory, int allowedMem ,AllocPart* AllocTableFree)
 {
+	int FreeArea = 0;
 	AllocTableFree     -> point = Memory;                      //запись в элемент массива структур указателя на начало новой свободной области
-	for (int FreeArea = 0;;)
+	for (FreeArea = 0; FreeArea < allowedMem ;)
 	{
-		if (Memory [FreeArea + 1] == 0) FreeArea++;         //если область все еще свободна, то продолжаем двигаться правее
-		else
+		if (Memory [FreeArea] == 0)
 		{
-			AllocTableFree     -> size = FreeArea;     //запись в элемент массива структур размера свободной области
-			return FreeArea;                           //возврат размера свободной области
+			FreeArea++;         //если область все еще свободна, то продолжаем двигаться правее
 		}
+		else
+			break;
 	}
+			AllocTableFree -> size = FreeArea;     //запись в элемент массива структур размера свободной области
+			return FreeArea;                           //возврат размера свободной области
+	
 }
 
 //---------------------------------------------------------------------
@@ -412,24 +448,28 @@ int IsEmployed (int* Memory)
 
 void AllocTab (int* Memory, int SizeOfMemory, AllocPart* AllocTableEmployed, AllocPart* AllocTableFree, int* Amount_of_mem_parts)
 {
+	
 	int FreeAreaNumber = 0;
 	int EmployedAreaNumber = 0;
 
 	for (int MemoryNumber = 0; MemoryNumber < SizeOfMemory;)
 	{
-		if (IsEmployed (&Memory [MemoryNumber]))
-		{
-			int EmployedArea = EnterValuesEmployed (&Memory [MemoryNumber], AllocTableEmployed + EmployedAreaNumber);
+		
+		if (IsEmployed (Memory + MemoryNumber)) 
+		{	
+			int EmployedArea = EnterValuesEmployed (Memory + MemoryNumber, SizeOfMemory - MemoryNumber ,AllocTableEmployed + EmployedAreaNumber);
+            
 			EmployedAreaNumber++;
 			MemoryNumber += EmployedArea;
 		}
 		else
-		{
-			int FreeArea = EnterValuesFree (&Memory [MemoryNumber], AllocTableFree + FreeAreaNumber);
+		{    
+			int FreeArea = EnterValuesFree (Memory + MemoryNumber, SizeOfMemory - MemoryNumber,AllocTableFree + FreeAreaNumber);
+        
 			FreeAreaNumber++;
 			MemoryNumber += FreeArea;
-		}
-
+		} 
+			
 	}
 
 	Amount_of_mem_parts[0] = EmployedAreaNumber;
@@ -452,7 +492,7 @@ int GayCompareAllocTable (AllocPart* MemorySet1, AllocPart* MemorySet2)
     int Size1 =  MemorySet1 -> size;
     int Size2 =  MemorySet2 -> size;
 
-    return (Size1 > Size2) - (Size1 < Size2);
+    return (Size1 > Size2);
 }
 
 
@@ -461,34 +501,110 @@ int GayCompareAllocTable (AllocPart* MemorySet1, AllocPart* MemorySet2)
 //----------------------------------------------------------------------
 void GaySwapAllocPart (AllocPart *MemoryArea_1, AllocPart *MemoryArea_2)
 {
-    AllocPart*  swap_const = MemoryArea_1;
-            	MemoryArea_1 = MemoryArea_2;
-            	MemoryArea_2 = swap_const;
+    AllocPart   swap_const    = *MemoryArea_1;
+            	*MemoryArea_1 = *MemoryArea_2;
+            	*MemoryArea_2 = swap_const;
 }
 
 
 //---------------------------------------------------------------------
 //гей-сортировка
 //---------------------------------------------------------------------
-void GaySortAllocTable (AllocPart* AllocTable, int Size)
+void GaySortAllocTable (AllocPart* AllocTable, int left, int right)
 {
-    int       i          = Size / 2;
-    AllocPart* ptr_left  = AllocTable + 1;
-    AllocPart* ptr_right = AllocTable + Size - 1;
-    AllocPart* ptr_pivot = AllocTable + i;
+    
+    int i = left;
+    int j = right;
+    int pivot = (right + left) / 2;
 
-    for(unsigned long long int k = 0, j = Size - 1; k < j; i++, j--)
+    //printf("pivot = %d\n", pivot);
+    //printf("at start: i = %d, j = %d\n", i, j);
+
+    do
     {
-        while(GayCompareAllocTable(ptr_left, ptr_pivot))
-            ptr_left++;
-        while(GayCompareAllocTable(ptr_pivot, ptr_right))
-            ptr_right--;
-
-        GaySwapAllocPart (ptr_left, ptr_right);
+        //printf("\nI am here\n");
+        //printAlloctable(AllocTable, Size);
+        while(GayCompareAllocTable(AllocTable + i, AllocTable + pivot) && (i < right) )
+            i++;
+        while(GayCompareAllocTable(AllocTable + pivot, AllocTable + j) && (j > left) )
+            j--;
+        if (i <= j)
+        {
+            //printf("i = %d, j = %d\n", i, j);
+            GaySwapAllocPart (AllocTable + i, AllocTable + j);
+            printAlloctable(AllocTable, right - left + 1);
+            //printf("\n\n");
+            i++;
+            j--;
+        }
     }
-    GaySortAllocTable (AllocTable, i);
-    GaySortAllocTable (AllocTable + i, Size - i);
+    while (i <= j);
+    if (j > left)
+        GaySortAllocTable (AllocTable, left, j);
+    if (i < right)
+    	GaySortAllocTable (AllocTable, i, right);
 }
+
+
+
+int TaskCompareAllocTable (Task* Task1, Task* Task2)
+{
+    int queue_num1 =  Task1 -> time_wait;
+    int queue_num2 =  Task2 -> time_wait;
+
+    return (queue_num1 < queue_num2);
+}
+
+
+//-----испр-------------------------------------------------------------
+//swap двух указанных элементов для гей-сортировки
+//----------------------------------------------------------------------
+void TaskSwapAllocPart (Task* Task1, Task* Task2)
+{
+    Task swap_const = *Task1;
+        	 *Task1 = *Task2;
+        	 *Task2 = swap_const;
+}
+
+
+//---------------------------------------------------------------------
+//гей-сортировка
+//---------------------------------------------------------------------
+void TaskSortAllocTable (Task* taskarr, int left, int right)
+{
+    
+    int i = left;
+    int j = right;
+    int pivot = (right + left) / 2;
+
+    printf("pivot = %d\n", pivot);
+    //printf("at start: i = %d, j = %d\n", i, j);
+
+    do
+    {
+        //printf("\nI am here\n");
+        //printAlloctable(AllocTable, Size);
+        while(TaskCompareAllocTable(taskarr + i, taskarr + pivot) && (i < right) )
+            i++;
+        while(TaskCompareAllocTable(taskarr + pivot, taskarr + j) && (j > left) )
+            j--;
+        if (i <= j)
+        {
+            //printf("i = %d, j = %d\n", i, j);
+            TaskSwapAllocPart (taskarr + i, taskarr + j);
+            //printf("\n\n");
+            i++;
+            j--;
+        }
+    }
+    while (i <= j);
+    if (j > left)
+        TaskSortAllocTable (taskarr, left, j);
+    if (i < right)
+    	TaskSortAllocTable (taskarr, i, right);
+}
+
+
 
 //-----------------------------------------------------------------------------
 // Данная фигня должна в уже отсортированном по размеру массиве AllocTable
@@ -502,7 +618,7 @@ void PrepforAddressSort (AllocPart* AllocTable, int Memsize)
 	{
 		int same_size = 1; 										// счетчик кусков с одинаковым размером, самый первый кусок сразу считаем
 
-		while ( (AllocTable[i].size == AllocTable[i + 1].size))  // смотрим, является ли следующий кусок одинаковым по размеру
+		while ( AllocTable[i].size == AllocTable[i + 1].size)  // смотрим, является ли следующий кусок одинаковым по размеру
 		{
 			same_size++;										// Если да, то увеличиваем счетчик
 		}
@@ -622,10 +738,15 @@ int main()
     int TaskNum = 0; // n - общее количество задач
     int t = 0; // t - время, на протяжении которого выполняется симуляция(в условных единицах)
 
-    int* Memory = (int *)calloc(1, Memsize * sizeof(char));  // макет памяти, 1 единица памяти в нашей ОС - 1 int
-    scanf("%d%d%d", &Memsize, &TaskNum, &t);
+    scanf("%d %d %d", &Memsize, &TaskNum, &t);
+
+	int* Memory = (int *)calloc(1, Memsize * sizeof(int));  // макет памяти, 1 единица памяти в нашей ОС - 1 int
 
     Task * StructArray = EnterTask(TaskNum);
+
+	printMemory(Memory, Memsize);
+
+	TaskSortAllocTable(StructArray, 0, TaskNum - 1);
 
     // функция создает лист на ожидание, пихает все задачи в него и возвращает его
     List * wait_list = wait_list_constructor(TaskNum, StructArray, Memsize, t);
@@ -639,35 +760,71 @@ int main()
 
     int Amount_of_mem_parts [2] = { 0 }; // 1 элемент - количество кусков занятой памяти, 2 - количество кусков свободной памяти
 
+	Node* new = wait_list -> head;
+
+	int Taskleft = TaskNum;
+
     while(1)
     {
-        Task * another_one = wait_list -> head -> task;
-        for (int i = 0; i < TaskNum; i++)
+		int i = 0;
+
+        
+        for (i = 0; i < Taskleft; i++)
         {
-            if (another_one -> mem > AllocTableFree -> size)
+			Task * another_one = new -> task;
+            if ( (another_one -> mem) < (AllocTableFree -> size) )
             {
+	
                 to_add_to_execution (another_one, todo_list, wait_list);
-                *(AllocTableFree -> point) = 1;
+
+				printMemory(Memory, Memsize);
+
+				printf("mem of another one = %d\n", another_one -> mem);
+
+				for (int j = 0; j < (another_one -> mem) ; j++)
+				{
+					//printf("\nin cycle\n");
+					AllocTableFree[0].point[j] = 1;
+					//printf("adding a bit\n");
+				}
+                	
+				another_one->taken_mem = AllocTableFree->point;
+
+
                 another_one -> status = 1;
 
+				printAlloctable(AllocTableFree, Amount_of_mem_parts[1]);
+				
                 AllocTab (Memory, Memsize, AllocTableEmployed, AllocTableFree, Amount_of_mem_parts); // переформируем куски свободной и заянтой памяти
 
-                GaySortAllocTable (AllocTableEmployed, Amount_of_mem_parts[0]); //пересортируем куски
-                GaySortAllocTable (AllocTableFree, Amount_of_mem_parts[1]);     // занятой и свободной памятей
+				printAlloctable(AllocTableFree, Amount_of_mem_parts[1]);
+
+                GaySortAllocTable (AllocTableEmployed, 0, Amount_of_mem_parts[0]);  //пересортируем куски
+                GaySortAllocTable (AllocTableFree, 0, Amount_of_mem_parts[1]);      //занятой и свободной памятей
             }
+
+			if (new -> next)
+				new = new -> next;
         }
+
+		Taskleft -= i;
+
 
         // исполняем одну задачу
         execution (todo_list -> head -> task, todo_list, &t);
 
+		printMemory(Memory, Memsize);
+
         // удаляем выполненную задачу из ОП и листа на исполнения
-        to_delete_a_task (todo_list -> head -> task, todo_list);
+        //to_delete_a_task (todo_list -> head -> task, todo_list);
+
+
 
         // прерывание цикла если истекло время, либо если все задачи выполнены
         if(t <= 0)
             break;
 
-        if(todo_list -> head == 0 && todo_list -> tail == 0)
+        if(wait_list -> head == 0 && wait_list -> tail == 0 && todo_list -> head ==  0 && todo_list -> tail == 0)
             break;
     }
 
@@ -677,6 +834,7 @@ int main()
 
     while(1)
     {
+		printf("\nВведите pid интересующей вас задачи или 0 в качестве отказа\n");
         scanf("%d", &pid);
 
         if(pid == 0)
